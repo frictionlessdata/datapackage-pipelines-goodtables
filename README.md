42001b469e8d3e68768c93644f62be61943a2c65
Brook Elgie <specialbrew@gmail.com>
2017-09-04 12:18:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Goodtables validation plugin for datapackage-pipelines
+
+[![Travis](https://img.shields.io/travis/frictionlessdata/datapackage-pipelines-goodtables/master.svg)](https://travis-ci.org/frictionlessdata/datapackage-pipelines-goodtables)
+[![Coveralls](http://img.shields.io/coveralls/frictionlessdata/datapackage-pipelines-goodtables/master.svg)](https://coveralls.io/r/frictionlessdata/datapackage-pipelines-goodtables?branch=master)
+[![PyPi](https://img.shields.io/pypi/v/datapackage-pipelines-goodtables.svg)](https://pypi.python.org/pypi/datapackage-pipelines-goodtables)
+[![SemVer](https://img.shields.io/badge/versions-SemVer-brightgreen.svg)](http://semver.org/)
+[![Gitter](https://img.shields.io/gitter/room/frictionlessdata/chat.svg)](https://gitter.im/frictionlessdata/chat)
+
+## Install
+
+```
+# clone the repo and install it with pip
+
+git clone https://github.com/frictionlessdata/datapackage-pipelines-goodtables.git
+pip install -e .
+```
+
+## Usage
+
+
---
+++ datapackage_pipelines_goodtables/processors/validate.py
@@ -0,0 +1,78 @@
+import itertools
+import json as json_module
+
+from datapackage_pipelines.wrapper import ingest, spew
+from goodtables import validate
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def _log_report(report, json=False, fail_on_error=True, fail_on_warn=False):
+
+    def _split_json_log(json_log, level=logging.INFO, line_limit=None):
+        '''Dumps json to an indented string, splits at new lines, then logs
+        each line out to `level`, optionally limiting line length to
+        `line_limit`.'''
+        json_log_dumps = json_module.dumps(json_log, indent=4)
+        [log.log(level, l[:line_limit]) for l in json_log_dumps.splitlines()]
+
+    if json:
+        return log.info(json_module.dumps(report, indent=4))
+    tables = report.pop('tables')
+    warnings = report.pop('warnings')
+    has_errors = False
+
+    log.info('DATASET')
+    log.info('='*7)
+
+    _split_json_log(report)
+
+    if warnings:
+        log.info('-'*9)
+        log.info('Table Warnings:')
+        for warning in warnings:
+            _split_json_log(warning, level=logging.WARN, line_limit=128)
+
+    for table_number, table in enumerate(tables, start=1):
+        log.info('TABLE [%s]' % table_number)
+        log.info('='*9)
+        errors = table.pop('errors')
+        _split_json_log(table, line_limit=128)
+        if errors:
+            has_errors = True
+            log.info('-'*9)
+            for error in errors:
+                error = {key: value or '-' for key, value in error.items()}
+                template = '[{row-number},{column-number}] [{code}] {message}'
+                message = template.format(**error)
+                log.error(message)
+
+    if (warnings and fail_on_warn) or (has_errors and fail_on_error):
+        raise RuntimeError('Datapackage failed Goodtables validation. '
+                           + 'See log for details.')
+
+
+parameters, datapackage, res_iter = ingest()
+
+fail_on_error = parameters['fail_on_error']
+fail_on_warn = parameters['fail_on_warn']
+
+
+def process_resources(res_iter_, datapackage):
+
+    def _validate_resource(res, schema):
+        evaluated_rows, rows = itertools.tee(res)
+        evaluated_rows = list(r._evaluate() for r in evaluated_rows)
+        report = validate(evaluated_rows, schema=schema, order_fields=False)
+        _log_report(report, False, fail_on_error, fail_on_warn)
+
+        yield from rows
+
+    for i, res in enumerate(res_iter_):
+        dp_res = datapackage['resources'][i]
+
+        yield _validate_resource(res, dp_res['schema'])
+
+
+spew(datapackage, process_resources(res_iter, datapackage))
